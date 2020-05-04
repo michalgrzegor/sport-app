7f308bab2da9d399b35bf098a89608097a5d9a81
michalgrzegor <michalgrzegor@gmail.com>
2020-05-04 08:17:56 +0200

    com -> app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,15 @@
 export const environment = {
   production: true,
-  stripeKey: 'pk_test_JHJ3HkTH3edd68bzsQpqNzPb00EA2Ro7D1',
+  stripeKey: "pk_test_JHJ3HkTH3edd68bzsQpqNzPb00EA2Ro7D1",
   push: {
-    publicKey:"BM8NBdhm3yA0-fJCf3rXmb2ih7TiuTF5ngj7Jj2ZDS04Gdjw-lu7wSdF5F4S0Pk4e__cn_hSig-m2tXfxNR7QUg",
-    privateKey:"iy4gYT1JE592nwH7wttT5M4Zqq5ns54OjCU7vT8Z6yM"
+    publicKey:
+      "BM8NBdhm3yA0-fJCf3rXmb2ih7TiuTF5ngj7Jj2ZDS04Gdjw-lu7wSdF5F4S0Pk4e__cn_hSig-m2tXfxNR7QUg",
+    privateKey: "iy4gYT1JE592nwH7wttT5M4Zqq5ns54OjCU7vT8Z6yM",
   },
   auth0: {
-    domain: 'dev-9ej1r9oe.auth0.com',
-    clientID: '7y6PXMSrRgdQDJHOlHxJa93siPe7olM5',
-    callbackURL: 'https://happy-galileo-b5f375.netlify.com/login',
-    audience: "https://ccoach-app.herokuapp.com/"
-  }
+    domain: "dev-9ej1r9oe.auth0.com",
+    clientID: "7y6PXMSrRgdQDJHOlHxJa93siPe7olM5",
+    callbackURL: "https://happy-galileo-b5f375.netlify.app/login",
+    audience: "https://ccoach-app.herokuapp.com/",
+  },
 };
